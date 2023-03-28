d039fc34f5d472bf7de3bf597f8787e01c849cc5
CJYjob <cjyjob1993@gmail.com>
2023-03-28 16:41:30 +0900

    complete log.py

--- log.py
+++ log.py
@@ -1,15 +1,26 @@
 from datetime import datetime
 import random
+import time
 
-# IP            Time                    URL                        Status
-# 10.128.2.1    29/Nov/2017:06:58:55   GET /login.php HTTP/1.1     200
+
+
+STAUS = [200, 200, 200,200, 200, 200, 200, 200, 200, 200, 200, 200, 206, 302, 304, 404, 500]
+
 def create_log():
-    IP = '.'.join(map(lambda x: str(random.randrange(1,256)), range(4)))
-    TIME = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    URL = random.choice(["GET", "POST"])
-    LOG = f"'{IP}', '{TIME}', '{URL}'"
-    return LOG
+  with open('url.txt') as f:
+    randomLine = random.choice(list(f.readlines()))
+    f.close()
 
-if __name__ == '__main__' :
-    for i in range(10) :
-        print(create_log())
+  IP = '.'.join(map(lambda x:str(random.randrange(1, 255)), range(4)))
+  Time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+  URL = randomLine.splitlines()[0]
+  Staus = random.choice(STAUS)
+  return IP, Time, URL, Staus
+
+# IP              Time                    URL                    Staus
+# 10.128.2.1   29/Nov/2017 06:58:55   GET /login.php HTTP/1.1   200
+
+while True:
+  print(create_log())
+
+  time.sleep(1)
